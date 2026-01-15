ca2fafdb578e89a4f30589d4fcbac6da3811b360
yengkong76543 <yengkong76543@gmail.com>
2026-01-14 20:24:44 -0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,4 +1,23 @@
-from app import app
+from flask import Flask, request, jsonify
+import requests, os
 
-def handler(request, context):
-    return app
+app = Flask(__name__)
+
+SUPABASE_URL = os.environ["https://supabase.com/"]
+SUPABASE_KEY = os.environ["sb_publishable_71sOetWHfO8f3vy0l6x1dQ_2zv77QKb"]
+
+@app.route("/", methods=["POST"])
+def collect_fingerprint():
+    data = request.get_json()
+
+    r = requests.post(
+        f"{SUPABASE_URL}/rest/v1/fingerprint",
+        headers={
+            "apikey": SUPABASE_KEY,
+            "Authorization": f"Bearer {SUPABASE_KEY}",
+            "Content-Type": "application/json",
+            "Prefer": "return=minimal"
+        },
+        json={"data": data}
+    )
+    return jsonify({"ok": True})
